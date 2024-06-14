836d5459d658b50c17c3a4b0135e8544c507652e
pavankumar <pavankumarkosaraju@gmail.com>
2024-06-14 16:01:55 +0530

    refactor: Remove unused code and update metadata in layout.tsx

---
+++ constants/index.ts
@@ -0,0 +1,15 @@
+export const sidebarLinks = [
+{
+    route : '/',
+    label: 'Home',
+    imgURL: '/icons/home.svg'
+},{
+    route : '/discover',
+    label: 'Discover',
+    imgURL: '/icons/discover.svg'
+},{
+    route : '/create-podcast',
+    label: 'create Podcast',
+    imgURL: '/icons/microphone.svg'
+}];
+
